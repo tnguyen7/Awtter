913070664569317b17e2a610a6f3a23c571443d1
rgvital <richelle.vital@gmail.com>
2015-06-26 20:24:34 -0700

    the camera workssss

--- app/src/main/java/com/example/tina/awtter/MainActivity.java
+++ app/src/main/java/com/example/tina/awtter/MainActivity.java
@@ -1,23 +1,35 @@
 package com.example.tina.awtter;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     View content;
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private Uri fileUri;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,19 +100,80 @@ public class MainActivity extends AppCompatActivity {
             case (R.id.action_add): {
 
                 // TODO
-                // openAdd();
+               useCamera();
                 return true;
 
-                // If settings was selected, ...
-            /*} case (R.id.action_settings): {
-
-                // TODO
+            }
+            case (R.id.action_settings): {
                 // openSettings();
-                return true;*/
+                return true;
 
             }
         }
 
         return super.onOptionsItemSelected(item);
+    }
+/*
+    private boolean openAdd(Menu item) {
+        switch (item.getItemId()) {
+
+        }
+
+    }*/
+
+    private boolean useCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
+
+        // start the image capture Intent
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+
+        return true;
+    }
+
+
+    /** Create a file Uri for saving an image or video */
+
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /** Create a File for saving an image or video */
+
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        /* DOESN'T WORK ATM D:
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }*/
+
+        // Create a media file name
+
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_" + ".jpg");
+        }/* else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ ".mp4");*/
+        else {
+            return null;
+        }
+
+        return mediaFile;
     }
 }
