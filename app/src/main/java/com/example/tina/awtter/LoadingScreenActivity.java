c7d4a4d6badefbdbe25cb675862b494ed86abc33
Tina <tina7nguyen@gmail.com>
2015-06-26 22:28:26 -0700

    Fragments

--- app/src/main/java/com/example/tina/awtter/LoadingScreenActivity.java
+++ app/src/main/java/com/example/tina/awtter/LoadingScreenActivity.java
@@ -1,11 +1,17 @@
 package com.example.tina.awtter;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
 
 public class LoadingScreenActivity extends Activity {
+    LoadingSurfaceView loadingSurfaceView;
 
     //Introduce an delay
     private final int WAIT_TIME = 2500;
@@ -13,8 +19,11 @@ public class LoadingScreenActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-        System.out.println("LoadingScreenActivity  screen started");
+        System.out.println("LoadingScreenActivity screen started");
         setContentView(R.layout.loading_screen);
+
+        //loadingSurfaceView = new LoadingSurfaceView(this);
+        //setContentView(loadingSurfaceView);
 
         new Handler().postDelayed(new Runnable(){
             @Override
@@ -32,5 +41,36 @@ public class LoadingScreenActivity extends Activity {
                 LoadingScreenActivity.this.finish();
             }
         }, WAIT_TIME);
+    }
+}
+
+class LoadingSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+
+    public LoadingSurfaceView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onSizeChanged (int w, int h, int oldw, int oldh) {
+
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 }
--- app/src/main/java/com/example/tina/awtter/MainActivity.java
+++ app/src/main/java/com/example/tina/awtter/MainActivity.java
@@ -1,69 +1,93 @@
 package com.example.tina.awtter;
 
-import android.app.Activity;
+import android.net.Uri;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements HomeFragment.OnFragmentInteractionListener,
+        FavoritesFragment.OnFragmentInteractionListener, MyPicturesFragment.OnFragmentInteractionListener,
+        SettingsFragment.OnFragmentInteractionListener {
+    final String HOME = "Home";
+    final String FAVORITE = "Favorites";
+    final String MY_PICTURES = "My Pictures";
+    final String SETTINGS = "Settings";
     DrawerLayout drawerLayout;
     View content;
+    HomeFragment homeFragment;
+    FavoritesFragment favoritesFragment;
+    MyPicturesFragment myPicturesFragment;
+    SettingsFragment settingsFragment;
+
+    Fragment currentFragment;
+
+    String title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        // Set content view to actual content/FrameLayout
-        content = findViewById(R.id.content);
+            setContentView(R.layout.activity_main);
 
-        // Enable toolbar as action bar
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        final ActionBar actionBar = getSupportActionBar();
+            setUpToolbar();
 
-        // Set up home indicator and enable home as up
-        if (actionBar != null) {
-            // Assing icon for home
-            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
-            actionBar.setDisplayHomeAsUpEnabled(true);
+            setUpDrawer();
+
+
+        currentFragment = getSupportFragmentManager().findFragmentByTag(getString(R.string.home));
+
+        if (currentFragment == null) {
+
+            homeFragment = new HomeFragment();
+            favoritesFragment = new FavoritesFragment();
+            myPicturesFragment = new MyPicturesFragment();
+            settingsFragment = new SettingsFragment();
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.content, settingsFragment, getString(R.string.settings))
+                    .hide(settingsFragment)
+                    .add(R.id.content, myPicturesFragment, getString(R.string.my_pictures))
+                    .hide(myPicturesFragment)
+                    .add(R.id.content, favoritesFragment, getString(R.string.favorite))
+                    .hide(favoritesFragment)
+                    .add(R.id.content, homeFragment, getString(R.string.home))
+                    .commit();
+            currentFragment = homeFragment;
+        } else {
+
+            homeFragment = (HomeFragment) currentFragment;
+            myPicturesFragment = (MyPicturesFragment) getSupportFragmentManager().findFragmentByTag(getString(R.string.my_pictures));
+            favoritesFragment = (FavoritesFragment) getSupportFragmentManager().findFragmentByTag(getString(R.string.favorite));
+            settingsFragment = (SettingsFragment) getSupportFragmentManager().findFragmentByTag(getString(R.string.settings));
+
+            getSupportFragmentManager().beginTransaction()
+                    .hide(settingsFragment)
+                    .hide(myPicturesFragment)
+                    .hide(favoritesFragment)
+                    .commit();
+
         }
 
-        // Initalize Navigation drawer
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        // Initialize NavigationView
-        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
-
-
-
-        // Set up listener so that when a person clicks on an item
-        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem) {
-
-                // Make snackbar on content(FrameLayout) view on teh bottom
-                Snackbar.make(content, menuItem.getTitle() + " pressed", Snackbar.LENGTH_LONG).show();
-
-                // Make it checked/highlighted when pressed
-                menuItem.setChecked(true);
-
-                // Close the drawer
-                drawerLayout.closeDrawers();
-
-                return true;
-            }
-        });
     }
 
     @Override
@@ -78,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Switch to see which item was selected from the action bar
         switch (item.getItemId()) {
+
             // If hamburger was selected, open the drawer
             case android.R.id.home: {
 
@@ -88,19 +113,173 @@ public class MainActivity extends AppCompatActivity {
             case (R.id.action_add): {
 
                 // TODO
-                // openAdd();
+                //   openAdd();
                 return true;
 
                 // If settings was selected, ...
-            /*} case (R.id.action_settings): {
+            }
+            case (R.id.action_settings): {
 
                 // TODO
                 // openSettings();
-                return true;*/
+                return true;
 
             }
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void setUpToolbar() {
+        // Enable toolbar as action bar
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+
+        // Set up home indicator and enable home as up
+        if (actionBar != null) {
+            // Assing icon for home
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public void setUpDrawer() {
+
+        // Set content view to actual content/FrameLayout ( for snackbar )
+        content = findViewById(R.id.content);
+
+        // Initialize Navigation drawer
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        // Initialize NavigationView
+        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
+
+
+        // Set up listener so that when a person clicks on an item
+        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                title = menuItem.getTitle().toString();
+
+                switch (title) {
+
+                    case (HOME): {
+
+                        switchTo(homeFragment, title);
+                        break;
+
+                    }
+                    case (FAVORITE): {
+
+                        switchTo(favoritesFragment, title);
+                        break;
+
+                    }
+                    case (MY_PICTURES): {
+
+                        switchTo(myPicturesFragment, title);
+                        break;
+
+                    }
+                    case (SETTINGS): {
+
+                        switchTo(settingsFragment, title);
+                        break;
+
+                    }
+
+                }
+                // Make snackbar on content(FrameLayout) view on teh bottom
+                //Snackbar.make(content, menuItem.getTitle() + " pressed", Snackbar.LENGTH_LONG).show();
+
+                // Make it checked/highlighted when pressed
+                menuItem.setChecked(true);
+
+                // Close the drawer
+                drawerLayout.closeDrawers();
+
+                return true;
+            }
+
+        });
+
+    }
+
+    void switchTo (Fragment fragment, String name) {
+        if (fragment.isVisible())
+            return;
+
+        FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+        t.setCustomAnimations(R.anim.slide_in,
+                R.anim.slide_out);
+
+        // Make sure the next view is below the current one
+        fragment.getView().bringToFront ();
+        // And bring the current one to the very top
+        currentFragment.getView().bringToFront ();
+
+        // Hide the current fragment
+        t.hide (currentFragment);
+        t.show(fragment);
+        currentFragment = fragment;
+
+        // You probably want to add the transaction to the backstack
+        // so that user can use the back button
+        t.addToBackStack(null);
+        t.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
+
+class JSONfunctions {
+
+
+    public static JSONObject getJSONfromURL(String url) {
+        HttpURLConnection conn = null;
+        String result = "";
+        JSONObject jArray = null;
+        InputStream is = null;
+
+        try {
+            conn = (HttpURLConnection) new URL("http://sql3.freemysqlhosting.net").openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            is = conn.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Convert response to string
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            result = sb.toString();
+        } catch (Exception e) {
+            Log.e("log_tag", "Error converting result " + e.toString());
+        }
+
+        try {
+
+            jArray = new JSONObject(result);
+        } catch (JSONException e) {
+            Log.e("log_tag", "Error parsing data " + e.toString());
+        }
+
+        return jArray;
     }
 }
