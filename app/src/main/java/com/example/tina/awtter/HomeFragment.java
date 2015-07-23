d5c1c833604f9ded1ee9c6f3f68280f25e900d03
Tina <tina7nguyen@gmail.com>
2015-07-22 19:01:49 -0700

    Combined recycler and home fragment

--- app/src/main/java/com/example/tina/awtter/HomeFragment.java
+++ app/src/main/java/com/example/tina/awtter/HomeFragment.java
@@ -4,11 +4,16 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.internal.widget.AdapterViewCompat;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +24,7 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -28,6 +34,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -57,8 +67,32 @@ public class HomeFragment extends Fragment {
 
     ArrayList<HashMap<String, String>> animalsList;
     ListView lv;
+    String startPoint;
 
-    String startPoint = "1";
+
+
+    List<Animal> animals;
+    Animal[] posts = new Animal[3];
+    HashMap<Boolean, Bitmap> porOrLan = new HashMap<Boolean, Bitmap>();
+
+    public int height;
+    public int width;
+    private boolean loading = true;
+    int pastVisiblesItems, visibleItemCount, totalItemCount;
+    ArrayList<HashMap<String, String>> threeAnimals;
+
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_FILTER = "filter";
+    private static final String TAG_START_POINT = "start";
+    private static final String TAG_ID = "__id";
+    private static final String TAG_UPAWS = "__upAws";
+    private static final String TAG_CREATEDAT = "__createdAt";
+    private static final String TAG_ANIMALS = "animals";
+    private static final String url = "http://76.244.35.83/media/";
+
+
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -73,27 +107,77 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.recycler, container, false);
 
-        /* Test Picasso
-
-        //Initialize ImageView
-        ImageView imageView = (ImageView) view.findViewById(R.id.content);
-
-        //Loading image from below url into imageView
-
-        Picasso.with(getActivity())
-                .load("http://76.244.35.83/images/grapefruit")
-                .into(imageView);*/
-
-        // Hashmap for ListView
         animalsList = new ArrayList<HashMap<String, String>>();
-        // Get listview
-        lv = (ListView) view.findViewById(R.id.listView);
-        // Loading products in Background Thread
+
         new LoadAnimals().execute();
 
 
+
+
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        ((Activity)context).getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        height = displaymetrics.heightPixels;
+        width = displaymetrics.widthPixels;
+
+        initializeData();
+
+        RecyclerView rv = (RecyclerView) view.findViewById(R.id.rv);
+
+        final GridLayoutManager glm = new GridLayoutManager(context, 3);
+        rv.setLayoutManager(glm);
+
+        glm.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                Log.v("ADAPT", "SIZEORIENT = " + animals.get(position).sizeOrient);
+                switch (animals.get(position).sizeOrient) {
+                    case 1:
+                        return 1;
+                    case 2:
+                        return 2;
+                    case 3:
+                        return 3;
+                    default:
+                        return 0;
+                }
+            }
+        });
+
+        RVAdapter adapter = new RVAdapter(animals, context, glm);
+        rv.setAdapter(adapter);
+        SpacesItemDecoration spaces = new SpacesItemDecoration(13, animals);
+        rv.addItemDecoration(spaces);
+
+
+        rv.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+
+                visibleItemCount = glm.getChildCount();
+                totalItemCount = glm.getItemCount();
+                pastVisiblesItems = glm.findFirstVisibleItemPosition();
+
+                if (loading) {
+                    if ((visibleItemCount + pastVisiblesItems) >= totalItemCount) {
+                        loading = false;
+                        Log.v("...", "Last Item Wow !");
+                    }
+                }
+            }
+        });
+
+
+        rv.addOnItemTouchListener( // and the click is handled
+                new RecyclerClickListener(context, new RecyclerClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
+                        // STUB:
+                        // The click on the item must be handled
+                        Toast.makeText(context, "itemclick: " + position, Toast.LENGTH_SHORT).show();
+
+                    }
+                }));
 
 
         // Inflate the layout for this fragment
@@ -139,6 +223,147 @@ public class HomeFragment extends Fragment {
         public void onFragmentInteraction(Uri uri);
     }
 
+    // This method creates an ArrayList that has three Animal objects
+    private void initializeData() {
+        this.animals = new ArrayList<>();
+        String id;
+        Bitmap bitmap;
+        int imageHeight, imageWidth;
+        String src;
+        for (int index = 0; index < threeAnimals.size(); index++) {
+            id = threeAnimals.get(0).get(TAG_ID);
+
+            src = url + id;
+
+            try {
+
+                //TODO: Move this to async task eventually
+                URL url = new URL(src);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                bitmap = BitmapFactory.decodeStream(input);
+
+                imageHeight = bitmap.getHeight();
+                imageWidth = bitmap.getWidth();
+                boolean isPortrait = false;
+
+                if(imageHeight > imageWidth) {
+                    isPortrait = true;
+                }
+                porOrLan[index] = isPortrait;
+
+            } catch (IOException e) {
+                // Log exception
+            }
+
+        }
+
+        reorganize();
+
+    }
+
+    private void reorganize() {
+        animals = new ArrayList<Animal>();
+        boolean firstRow = false;
+
+        if(porOrLan[0] == true && porOrLan[1] == true && porOrLan[2] == true) {
+            // PPP
+            Log.v("HERE", "P,P,P");
+            Bitmap icon = BitmapFactory.decodeResource(this.getResources(), R.drawable.cuteturtle);
+            animals.add(new Animal(icon, 1, true, false, true));
+            ///////////////////
+
+            animals.add(new Animal(BitmapFactory.decodeResource(this.getResources(), R.drawable.kappa), 2, true, true, true));
+
+            animals.add(new Animal(BitmapFactory.decodeResource(this.getResources(), R.drawable.richelle), 3, false, true, false));
+
+
+        } else if(porOrLan[0] == true && porOrLan[1] == true && porOrLan[2] == false) {
+            Log.v("HERE", "P1L,P2");
+            //P1 L
+            //P2
+        } else if(porOrLan[0] == true && porOrLan[1] == false && porOrLan[2] == false) {
+            Log.v("HERE", "PL,L");
+            //PL
+            //L
+
+            Bitmap icon = BitmapFactory.decodeResource(this.getResources(), R.drawable.cuteturtle);
+            animals.add(new Animal(icon, 1, true, false, true));
+
+            animals.add(new Animal(BitmapFactory.decodeResource(this.getResources(), R.drawable.kappa), 2, true, true, true));
+
+            animals.add(new Animal(BitmapFactory.decodeResource(this.getResources(), R.drawable.richelle), 3, false, true, true));
+
+
+        } else if(porOrLan[0] == true && porOrLan[1] == false && porOrLan[2] == true) {
+            Log.v("HERE", "PL,P3");
+            // PL
+            // P3
+            Bitmap icon = BitmapFactory.decodeResource(this.getResources(), R.drawable.cuteturtle);
+            animals.add(new Animal(icon, 1, true, false, true));
+
+            animals.add(new Animal(BitmapFactory.decodeResource(this.getResources(), R.drawable.kappa), 2, true, true, true));
+
+            animals.add(new Animal(BitmapFactory.decodeResource(this.getResources(), R.drawable.richelle), 3, false, false, true));
+
+
+        } else if(porOrLan[0] == false && porOrLan[1] == false && porOrLan[2] == false) {
+            Log.v("HERE", "L,L,L");
+            //L
+            //L
+            //L
+            Bitmap icon = BitmapFactory.decodeResource(this.getResources(), R.drawable.cuteturtle);
+            animals.add(new Animal(icon, 1, true, true, true));
+
+            animals.add(new Animal(BitmapFactory.decodeResource(this.getResources(), R.drawable.kappa), 2, false, true, true));
+
+            animals.add(new Animal(BitmapFactory.decodeResource(this.getResources(), R.drawable.richelle), 3, false, true, true));
+
+
+        } else if(porOrLan[0] == false && porOrLan[1] == false && porOrLan[2] == true) {
+            Log.v("HERE", "L,LP");
+            //L
+            //LP
+
+            Bitmap icon = BitmapFactory.decodeResource(this.getResources(), R.drawable.cuteturtle);
+            animals.add(new Animal(icon, 1, true, true, true));
+
+            animals.add(new Animal(BitmapFactory.decodeResource(this.getResources(), R.drawable.kappa), 2, false, true, true));
+
+            animals.add(new Animal(BitmapFactory.decodeResource(this.getResources(), R.drawable.richelle), 3, false, true, true));
+
+
+        } else if(porOrLan[0] == false && porOrLan[1] == true && porOrLan[2] == false) {
+            Log.v("HERE", "LP,L");
+            //LP
+            //L
+
+            Bitmap icon = BitmapFactory.decodeResource(this.getResources(), R.drawable.cuteturtle);
+            animals.add(new Animal(icon, 1, true, false, true));
+
+            animals.add(new Animal(BitmapFactory.decodeResource(this.getResources(), R.drawable.kappa), 2, true, true, true));
+
+            animals.add(new Animal(BitmapFactory.decodeResource(this.getResources(), R.drawable.richelle), 3, false, false, true));
+
+
+        } else if(porOrLan[0] == false && porOrLan[1] == true && porOrLan[2] == true) {
+            Log.v("HERE", "LP,P3");
+            //LP
+            //P3
+
+            Bitmap icon = BitmapFactory.decodeResource(this.getResources(), R.drawable.cuteturtle);
+            animals.add(new Animal(icon, 1, true, false, true));
+
+            animals.add(new Animal(BitmapFactory.decodeResource(this.getResources(), R.drawable.kappa), 2, true, true, true));
+
+            animals.add(new Animal(BitmapFactory.decodeResource(this.getResources(), R.drawable.richelle), 3, false, false, true));
+        }
+
+
+    }
+
     /**
      * Background Async Task to Load all product by making HTTP Request
      * */
@@ -156,15 +381,6 @@ public class HomeFragment extends Fragment {
         private String url_all_products = "http://76.244.35.83/get_animals.php";
 
         private HashMap<String, String> map;
-
-        // JSON Node names
-        private static final String TAG_SUCCESS = "success";
-        private static final String TAG_FILTER = "filter";
-        private static final String TAG_START_POINT = "start";
-        private static final String TAG_ID = "__id";
-        private static final String TAG_UPAWS = "__upAws";
-        private static final String TAG_CREATEDAT = "__createdAt";
-        private static final String TAG_ANIMALS = "animals";
 
         // products JSONArray
         JSONArray animals = null;
@@ -244,35 +460,14 @@ public class HomeFragment extends Fragment {
          * **/
         protected void onPostExecute(String file_url) {
 
-            ArrayList<HashMap<String, String>> threeAnimals = new ArrayList<HashMap<String, String>>();
+            threeAnimals = new ArrayList<HashMap<String, String>>();
             HashMap<String, String> animal1 = animalsList.get(0);
             HashMap<String, String> animal2 = animalsList.get(1);
             HashMap<String, String> animal3 = animalsList.get(2);
 
             threeAnimals.add(animal1);
             threeAnimals.add(animal2);
-            threeAnimals.add(animal3);
-
-            Intent intent = new Intent (context, RecyclerMainActivity.class);
-            intent.putExtra("3 animals", threeAnimals);
-            startActivity(intent);
-
-            // updating UI from Background Thread
-            runOnUiThread(new Runnable() {
-                public void run() {
-                    /**
-                     * Updating parsed JSON data into ListView
-                     * */
-                    ListAdapter adapter = new SimpleAdapter(
-                            context, animalsList,
-                            R.layout.list_item, new String[]{TAG_ID,
-                            TAG_UPAWS, TAG_CREATEDAT},
-                            new int[]{R.id.id, R.id.upAws, R.id.createdAt});
-                    // updating listview
-                    lv.setAdapter(adapter);
-                }
-            });
-
+            threeAnimals.add(animal3);;
 
 
         }
